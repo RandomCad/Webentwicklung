1b8dac686bbc1d58bb1907e0b833bd5b89d34ce4
RandomCad <mmont4850@gmail.com>
2024-10-10 11:35:53 +0200

    YS for hello world

--- Web/JS/helloWorld.js
+++ Web/JS/helloWorld.js
@@ -2,3 +2,12 @@
 document.write("<h1>Hello World</h1>");
 let test;
 test = 5;
+console.log("Hello");
+console.debug("Hello");
+console.info("Hello");
+console.warn("Hello");
+console.error("Hello");
+
+let name = prompt("Was ist ihr nahme");
+console.log(name);
+confirm("Hallo");
